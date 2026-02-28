fd70e17403199e659924d9e3874126ffd1c0af8b
Ekaterina1811-star <documents.obr.252@gmail.com>
2026-02-28 21:12:50 +0300

    urls для payment

---
+++ server/apps/payment/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from server.apps.payment.views import PaymentViewset
+
+
+router = DefaultRouter()
+
+
+router.register(r"payment", PaymentViewset)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
